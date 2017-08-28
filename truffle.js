fcd265083df5ecf9554233bc85202cf2902a1813
mahat <mehmet.ahat@gmail.com>
2017-08-28 23:31:44 +0300

    second commit

---
+++ truffle.js
@@ -0,0 +1,17 @@
+// Allows us to use ES6 in our migrations and tests.
+require('babel-register')
+
+module.exports = {
+  networks: {
+    development: {
+      host: 'bcllxxmvp.northeurope.cloudapp.azure.com',
+      port: 8545,
+      network_id: '*' // Match any network id
+    },
+    production: {
+      host: 'bcllxxmvp.northeurope.cloudapp.azure.com',
+      port: 8545,
+      network_id: '*' // Match any network id
+    }
+  }
+};
